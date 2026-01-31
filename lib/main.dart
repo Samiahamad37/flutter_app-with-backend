bf10bdf0e96aa8d889d8b0b0c17d429cbd21070e
Samia <hamadsamia37@gmail.com>
2026-02-01 00:25:59 +0300

    adding register fxn

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+// import 'package:demo_app/login.dart';
+// import 'package:demo_app/register.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+//     return const MaterialApp(debugShowCheckedModeBanner: false, home: RegisterPage());
+//   }
+// }
+
+
 import 'package:demo_app/login.dart';
 import 'package:demo_app/register.dart';
 import 'package:flutter/material.dart';
@@ -9,9 +29,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // First page shown
+      initialRoute: '/login',
+
+      // Register pages here
+      
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+      },
+    );
   }
 }
