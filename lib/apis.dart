f982ce079196edffa1a93d42cd9d57b974c86569
Samia <hamadsamia37@gmail.com>
2026-02-12 21:34:58 +0300

    learning app programm interface

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,6 +1,6 @@
-// model-structure of a particular resources  
-class Post{
-  const int userId;
-  int id
-} 
-// only static fields can be declared as const try declaring the final 
+// // model-structure of a particular resources  
+// class Post{
+//   const int userId;
+//   int id
+// } 
+// // only static fields can be declared as const try declaring the final 
